c4a7e08b9cb0baf67c81f077b9ec14006fee1ce7
Andrew Harper <druharper@gmail.com>
2020-02-26 13:55:55 -0600

    adding basic data file

---
+++ FishDataProvider.js
@@ -0,0 +1,44 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+  {
+    name: "Bart",
+    species: "Betta Fish",
+    location: "Panama",
+    length: 2.5,
+    food: ["algae", "Sea grass"]
+  },
+  {
+    name: "Rover",
+    species: "Nano Fish",
+    location: "Indonesia",
+    length: 3,
+    food: ["algae", "Spirulina"]
+  },
+  {
+    name: "Arabella",
+    species: "Swordtail",
+    location: "Caribbean",
+    length: 3.5,
+    food: ["Sea weed"]
+  },
+  {
+    name: "Jemma",
+    species: "Angelfish",
+    location: "Hawaii",
+    length: 2,
+    food: ["Spirulina", "krill"]
+  },
+  {
+    name: "Blue",
+    species: "Clownfish",
+    location: "Greece",
+    length: 3,
+    food: ["krill"]
+  }
+]
+
